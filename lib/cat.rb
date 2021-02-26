5a848fea4c84232b8233dab1b6deb76cbb5b87e7
RobertObi <robobi97@googlemail.com>
2021-02-26 00:50:39 -0600

    commit tree 09ef4fc07e020fbed3ae2c6da5714a6b9f52856b

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
-  # code goes here
+    attr_reader :name
+    attr_accessor :owner
+
+    @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+  end 
+
+  # def owner=(owner)
+  #   @owner
+  # end
+
+  # def name=(name)
+  #   @name
+  # end
+
+def self.all 
+  @@all
+end 
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
-end
+
+  attr_reader :name, :species, :count, :reset_all
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @count = count
+    @reset_all = reset_all
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.reset_all
+    @@all.clear
+   end 
+ 
+
+end 
+
+
